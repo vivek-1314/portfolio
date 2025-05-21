8e9625cc306a03f21a290ca32745e3c9bb356b04
vivek-1314 <vivek1314gurjar@gmail.com>
2025-05-21 09:40:54 +0530

    added meta tag for goole verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <SEO />
+      <head><meta name="google-site-verification" content="H4dxpfXKyBbPYuVq_KUqJ5ev8zF-Dd-oTjqhTu3BaAg" /></head>
       <body>
         {children}
       </body>
